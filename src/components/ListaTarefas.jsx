99a0a35b2c985873edd8bc091ebe7c623c1c989a
fhoinaski <sukinodoncai@gmail.com>
2023-03-17 22:21:28 -0300

    exercicio aula update

--- src/components/ListaTarefas.jsx
+++ src/components/ListaTarefas.jsx
@@ -3,33 +3,38 @@ import React, { useState } from 'react';
 
 // Definir o componente ListaTarefas
 const ListaTarefas = () => {
-    // Inicializar o estado das tarefas (array), item (string) e itens marcados (array)
-    const [tarefas, setTarefas] = useState([]);
-    const [item, setItem] = useState('');
-    const [checkedItems, setCheckedItems] = useState([]);
-    
+// Inicializar os estados das tarefas, item e próximo ID
+
+// 'tarefas' armazena a lista de tarefas e 'setTarefas' é a função para atualizar a lista de tarefas.
+const [tarefas, setTarefas] = useState([]);
+
+// 'item' armazena o valor do item atual (texto do input) e 'setItem' é a função para atualizar o valor do item.
+const [item, setItem] = useState('');
+
+// 'nextId' armazena o próximo ID disponível para um novo item e 'setNextId' é a função para atualizar o valor do próximo ID.
+const [nextId, setNextId] = useState(1);
+
 
     // Função para adicionar um item às tarefas
     const addItem = (e) => {
-        // Prevenir a ação padrão do formulário (recarregar a página)
         e.preventDefault();
-        // Atualizar a lista de tarefas, adicionando o novo item
-        setTarefas([...tarefas, { texto: item, concluido: false }]);
+        // Atualizar a lista de tarefas, adicionando o novo item com um ID único
+        setTarefas([...tarefas, { id: nextId, texto: item, concluido: false }]);
         // Limpar o campo de entrada
         setItem('');
-    }
+        // Incrementar o próximo ID disponível
+        setNextId(nextId + 1);
+    };
 
     // Função para alternar o estado concluído de um item
-    const toggleItem = (index) => {
-        // Criar uma cópia da lista atual de tarefas
-        const newLista = [...tarefas];
-        // Alternar o valor 'concluido' do item no índice fornecido
-        newLista[index].concluido = !newLista[index].concluido;
+    const toggleItem = (id) => {
+        // Mapear as tarefas e atualizar a tarefa com o ID correspondente
+        const newLista = tarefas.map((tarefa) =>
+            tarefa.id === id ? { ...tarefa, concluido: !tarefa.concluido } : tarefa
+        );
         // Atualizar o estado das tarefas
         setTarefas(newLista);
-        // Imprimir a lista de tarefas no console
-        console.log(tarefas);
-    }
+    };
 
     // Renderizar o componente
     return (
@@ -48,26 +53,43 @@ const ListaTarefas = () => {
                 {/* Botão para enviar o formulário e adicionar o item */}
                 <button type="submit">Adicionar</button>
             </form>
-            {/* Lista de itens */}
+            {/* Lista de tarefas */}
+            <h2>Tarefas</h2>
             <ul>
-                {/* Mapear as tarefas para elementos de lista (li) */}
-                {tarefas.map((itemObj, index) => (
-                    <li key={index}>
-                        {/* Adicionar um checkbox para controlar o estado concluído do item */}
-                        <input
-                            type="checkbox"
-                            checked={itemObj.concluido}
-                            // Chamar a função toggleItem quando o checkbox for marcado ou desmarcado
-                            onChange={() => toggleItem(index)}
-                        />
-                        {/* Exibir o texto do item */}
-                        {itemObj.texto}
-                    </li>
-                ))}
+                {/* Mapear e exibir as tarefas não concluídas */}
+                {tarefas
+                    .filter((itemObj) => !itemObj.concluido)
+                    .map((itemObj) => (
+                        <li key={itemObj.id}>
+                            <input
+                                type="checkbox"
+                                checked={itemObj.concluido}
+                                onChange={() => toggleItem(itemObj.id)}
+                            />
+                            {itemObj.texto}
+                        </li>
+                    ))}
+            </ul>
+            {/* Lista de tarefas concluídas */}
+            <h2>Tarefas Concluídas</h2>
+            <ul>
+                {/* Mapear e exibir as tarefas concluídas */}
+                {tarefas
+                    .filter((itemObj) => itemObj.concluido)
+                    .map((itemObj) => (
+                        <li key={itemObj.id}>
+                            <input
+                                type="checkbox"
+                                checked={itemObj.concluido}
+                                onChange={() => toggleItem(itemObj.id)}
+                            />
+                            {itemObj.texto}
+                        </li>
+                    ))}
             </ul>
         </div>
     );
-}
+};
 
 // Exportar o componente ListaTarefas
-export default ListaTarefas;
+export default ListaTarefas
